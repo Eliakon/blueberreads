754600c20ea008c34e72523f86e24a60d1300a45
Estelle Martinez <rikku.shimizu@gmail.com>
2017-09-13 20:26:19 +0200

    Create react app + amazing logo

---
+++ front/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Banner from './Banner';
+
+const App = () => (
+  <div className="App">
+    <Banner />
+  </div>
+);
+
+export default App;
---
+++ front/src/Banner/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Banner.css'
+
+const Banner = () => (
+  <header className="banner">
+    <img src="/images/logo.png" alt="Blueberreads" />
+  </header>
+)
+
+export default Banner;
