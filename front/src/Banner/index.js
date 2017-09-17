62bf3a109a49632e76b91c8f0df35f1ccd044710
Estelle Martinez <rikku.shimizu@gmail.com>
2017-09-17 18:33:13 +0200

    Begin post page integration

--- front/src/Banner/index.js
+++ front/src/Banner/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Link } from 'react-router-dom'
 
 import './Banner.css'
 
-const Banner = () => (
-  <header className="banner">
+const Banner = ({ size }) => (
+  <header className={`banner ${size ? `banner-${size}`: ''}`}>
     <Link to="/">
       <img src="/images/logo-large.png" alt="Blueberreads" />
     </Link>
   </header>
-)
+);
+
+Banner.propTypes = {
+  size: PropTypes.string,
+};
 
 export default Banner;
--- front/src/Post/index.js
+++ front/src/Post/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
 
-const Post = () => (
-  <div className="post">
-    <h1>Post</h1>
-  </div>
-);
+import Banner from '../Banner';
+
+import './Post.css'
+
+const data = {
+  id: 1,
+  slug: 'august-wrap-up',
+  title: 'August wrap-up',
+  date: '09-14-2017',
+  intro: 'I have read a total of four books this month. I did not expect to read so much, because I planned on reading The Final Empire which is huge. I nonetheless did it! I am quite proud of myself, let me tell you.',
+  books: ['/images/covers/15801353.jpg', '/images/covers/6547258.jpg', '/images/covers/17756559.jpg', '/images/covers/22037377.jpg'],
+};
+
+const Post = () => {
+  const { title, date, intro, books } = data;
+
+  return (
+    <div className="post">
+      <Banner size="small" />
+      <article className="content">
+        <header>
+          <div className="title">
+            <h1>{title}</h1>
+            <span className="date">16-09-2017</span>
+          </div>
+          <div className="books">
+            {books.map((coverUrl, n) => <img key={n} src={coverUrl} role="presentation" />)}
+          </div>
+        </header>
+      </article>
+    </div>
+  )
+};
 
 export default Post;
