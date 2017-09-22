b44921d3be5a9c2f1b2f1b925497aad5f710c616
Estelle Martinez <rikku.shimizu@gmail.com>
2017-09-22 17:17:09 +0200

    Comment form integration

--- front/src/Post/Comments/CommentForm/index.js
+++ front/src/Post/Comments/CommentForm/index.js
@@ -2,10 +2,25 @@ import React from 'react';
 
 import './CommentForm.css';
 
-const CommentForm = () => (
-  <div className="comment-form">
-    <h2>Write a comment</h2>
-  </div>
-);
+class CommentForm extends React.Component {
+  state = {
+    active: false,
+  };
+
+  render = () => (
+    <div className="comment-form">
+      <h2>Write a comment</h2>
+      <form className={this.state.active ? 'active': ''}>
+        <textarea onFocus={_ => this.setState({active: true})} placeholder="Your comment" />
+        <div className="contact">
+          <input className="required" type="text" placeholder="Name" />
+          <input type="text" placeholder="Website (optional)" />
+          <input type="text" placeholder="@twitter (optional)" />
+        </div>
+        <input className="submit" type="submit" value="Send" />
+      </form>
+    </div>
+  );
+};
 
 export default CommentForm;
